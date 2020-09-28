7a6b3b430f82ddc1824aed66afc4a37981d5c498
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-28 14:47:59 +0100

    Add the ability to pipe in input

--- src/wordwrap_cli.rb
+++ src/wordwrap_cli.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/env ruby
 
+# There are three ways of using this.
+# 1. Enter both inputs as command line params, like this: 
+#       ./src/wordwrap_cli.rb -t 'Hello world' -l 5
+# 2. Pipe in the text input, and add the length as a command line param, like this: 
+#       echo "Hello world" | ./src/wordwrap_cli.rb -l 5
+# 3. Give the length as a command line param, then you will be prompted to enter the text on the command line. Like this:
+#       ./src/wordwrap_cli.rb -l 5
+#       (Then enter Hello world on the command line afterwards)
+
+
 require_relative './wordwrap'
 require 'optparse'
 
@@ -20,5 +30,10 @@ end.parse!
 if options[:text] && options[:len]
     puts WordWrap.new.wrap(options[:text], options[:len])        
 else
-    puts "Please give values for --text (-t) and --len (-l)"  
+    stdinput = gets.chomp
+    if stdinput && options[:len]
+        puts WordWrap.new.wrap(stdinput, options[:len])
+    else
+        puts "Please give values for --text (-t) and --len (-l)"  
+    end
 end 
