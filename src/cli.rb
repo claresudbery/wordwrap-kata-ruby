102fb15cbcca9e62d5c4d24775cb79af8e1a4a2b
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-28 13:14:46 +0100

    don't output intro for help

--- src/cli.rb
+++ src/cli.rb
@@ -3,15 +3,6 @@
 require_relative './wordwrap'
 require 'optparse'
 
-puts "--------------"
-puts 'Welcome to Word Wrap'
-puts "--------------"
-puts "We want two inputs: A string and a line length. We'll wrap your text to your line length."
-puts "For instance the inputs 'hello world', 5 will output two lines - 'hello' and 'world' - like this:"
-puts "--------------"
-puts WordWrap.new.wrap("hello world", 5)
-puts "--------------"
-
 options = {}
 OptionParser.new do |parser|
     parser.on("-t", "--text=TEXT", "The text you want to wrap") do |value|
@@ -25,6 +16,15 @@ OptionParser.new do |parser|
         exit
     end
 end.parse!
+
+puts "--------------"
+puts 'Welcome to Word Wrap'
+puts "--------------"
+puts "We want two inputs: A string and a line length. We'll wrap your text to your line length."
+puts "For instance the inputs 'hello world', 5 will output two lines - 'hello' and 'world' - like this:"
+puts "--------------"
+puts WordWrap.new.wrap("hello world", 5)
+puts "--------------"
 
 if options[:text] && options[:len]
     puts WordWrap.new.wrap(options[:text], options[:len])        
